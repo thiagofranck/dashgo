3711bd44b0939a3b7238e3bd69dbb06f7d002047
Thiago Franck <thiagohenrique776@gmail.com>
2021-05-14 13:01:45 -0300

    Componente Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Th, Thead, Tr, Td, Text } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 
 import { Sidebar } from "../../components/Sidebar";
 
@@ -118,6 +119,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
