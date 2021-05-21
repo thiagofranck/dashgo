53421d50cc3ef80ac6e26c64b2f96cb51d222462
Thiago Franck <thiagohenrique776@gmail.com>
2021-05-21 11:15:14 -0300

    Sinalização de link ativo

---
+++ src/components/ActiveLink.tsx
@@ -0,0 +1,27 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { ReactElement, cloneElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement
+  shouldMatchExactHref?: boolean;
+}
+
+
+export function ActiveLink({ children, shouldMatchExactHref = false, ...rest }: ActiveLinkProps) {
+  const { asPath } = useRouter()
+
+  let isActive = false;
+
+  if (asPath === rest.href || asPath === rest.as) {
+    isActive = true;
+  }
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        color: isActive ? 'pink.400' : 'gray.50'
+      })}
+    </Link>
+  )
+}
