c53ab7a06e79fa5b236734646b667a4505bd3501
Thiago Franck <thiagohenrique776@gmail.com>
2021-05-21 12:47:41 -0300

    React hook form adicionado

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -13,9 +13,15 @@ export function ActiveLink({ children, shouldMatchExactHref = false, ...rest }: 
 
   let isActive = false;
 
-  if (asPath === rest.href || asPath === rest.as) {
+  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
     isActive = true;
   }
+
+  if (!shouldMatchExactHref && 
+    (asPath.startsWith(String(rest.href)) ||  
+     asPath.startsWith(String(rest.as)))) {
+       isActive = true;
+     }
 
   return (
     <Link {...rest}>
